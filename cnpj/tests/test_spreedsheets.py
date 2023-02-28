b6eab64b4e6ebfb41b923bac6aa213716d651199
Gabriel Reno <gabriel.meinberg@mediacaonline.com>
2023-02-28 15:00:17 -0300

    test: On create a file in test, delete on finish

--- cnpj/tests/test_spreedsheets.py
+++ cnpj/tests/test_spreedsheets.py
@@ -1,10 +1,20 @@
-from django.test import TestCase
+import shutil, tempfile
+
+from django.test import TestCase, override_settings
 from django.core.files.base import File
 from django.conf import settings
 
-from cnpj.models.spreedsheets import Spreedsheets
+from cnpj.models import Spreedsheets, CnpjCei
 
+MEDIA_ROOT = tempfile.mkdtemp()
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
 class TestSpreedsheets(TestCase):
+
+    @classmethod
+    def tearDownClass(cls): 
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+        super().tearDownClass()
 
     def setUp(self):
         file = open(f"{settings.BASE_DIR}/cnpj/tests/files/CNPJ DE MOGI- DEZEMBRO 2022 - RFB SMF.xlsx", 'rb')
@@ -12,4 +22,4 @@ class TestSpreedsheets(TestCase):
 
     def test_process(self):
         self.spreedsheets.process()
-        #TODO: Criar testes
+        self.assertEqual(CnpjCei.objects.count(), 171)
