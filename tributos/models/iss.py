7f0aac386fedf7a045e8812590cbc9dc021d0ebe
Gabriel Reno <gabriel.meinberg@mediacaonline.com>
2023-03-10 11:28:04 -0300

    chore: Changed label

--- tributos/models/iss.py
+++ tributos/models/iss.py
@@ -6,7 +6,7 @@ from tributos.models.choices.tipo_servico import TIPO_SERVICO
 class Iss(ModelBase):
     identification_number = models.CharField("Número de Identificação", max_length=14)
     iss_value = models.DecimalField("Valor ISS", max_digits=20, decimal_places=2)
-    base_calculo = models.DecimalField("Valor ISS", max_digits=20, decimal_places=2)
+    base_calculo = models.DecimalField("Base de Calculo", max_digits=20, decimal_places=2)
     period = models.DateField("Periodo de Apuracao", auto_now=False, auto_now_add=False)
     cod_atividade_cnae = models.CharField("Codigo da Atividade", max_length=8, null=True, blank=True)
     tipo_servico = models.IntegerField("Tipo de Serviço", choices=TIPO_SERVICO, null=True, blank=True)
